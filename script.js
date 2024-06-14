2656a591c687135c90f775fab41b87bc853a5ad8
chelangatbrigit <brigitchelangat@gmail.com>
2024-06-14 14:52:57 +0300

    Function definition and invokation

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 //variable declaration keyword var)
 var X = "Hello World!";
 var message = "hi";
+/*
+a) Function definition syntax
+1. function a ( Arguments){
+ -- function body --
+ }
+ 2. var a = function ( Arguments){
+ -- function body --
+ }
+-- Arguments are not declare with the keyword var
 
+ b) Function invokation or execution ways
+
+ 1. function compare (x,y) { --- }
+ 2. var a = compare(4,5);
+ 3. compare(4, "a");
+ 4. compare();
+ 
+ 
+*/
