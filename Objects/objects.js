262135e245af9a9ba642674b403ac59f3d3068aa
engenovic <engenovic@gmail.com>
2024-06-23 12:04:44 +0300

    Object literal syntax for object creation

--- Objects/objects.js
+++ Objects/objects.js
@@ -1,4 +1,6 @@
 //Object creation
+// 1. Using new Object() syntax
+console.log("1. Object creation using new Object() syntax");
 var company = new Object();
 company.name = "CodeAfrika Business Solutions";
 company.ceo = new Object();
@@ -8,3 +10,17 @@ company.ceo.favcolor ="Maroon";
 console.log(company);
 console.log(company["name"]);
 console.log("Company CEO name is: " + company.ceo.firstName +" " + company.ceo.lastName)
+
+// 2. Using Object Literal Syntax
+console.log("2. Object creation using Object Literal Syntax");
+var CodeAfrika = {
+    Name : "CodeAfrika Business Solutions",
+    ceo : {
+        firstName: "Ngeno",
+        lastName: "Victor",
+        favcolor: "Maroon"
+    },
+    "stock of company": 110
+};
+console.log(CodeAfrika);
+console.log(CodeAfrika.ceo.firstName);
