98bcfa990a5163f70d7ab83ff18bfb0e4b9d49e3
engenovic <engenovic@gmail.com>
2024-06-23 11:25:41 +0300

    Object creation with new Object()

---
+++ Objects/objects.js
@@ -0,0 +1,9 @@
+//Object creation
+var company = new Object();
+company.name = "CodeAfrika Business Solutions";
+company.ceo = new Object();
+company.ceo.firstName = "Ngeno";
+company.ceo.lastName = "Victor";
+company.ceo.favcolor ="Maroon";
+console.log(company);
+console.log("Company CEO name is: " + company.ceo.firstName +" " + company.ceo.lastName)
