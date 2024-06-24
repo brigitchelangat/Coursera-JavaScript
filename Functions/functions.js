1c1c10b9a485ffaf6ca0a19e0c727eb999f4aea1
chelangatbrigit <brigitchelangat@gmail.com>
2024-06-24 15:59:31 +0300

    Functions, Constructors, Prototype and the 'this' keyword

--- Functions/functions.js
+++ Functions/functions.js
@@ -26,3 +26,36 @@ console.log(b);
 b.x = 5;
 console.log(a);
 console.log(b);//Change in b affects a
+
+//Functions, Constructors, Prototype and the 'this' keyword
+function test(){
+    console.log("Hello CodeAfrika!");
+    console.log(this); //this keyword points to the global object Window
+    this.myName = "Brigit"
+}
+test();
+console.log(window.myName)
+
+//Function constructors
+function Circle (radius){ 
+    // Constructor- start with a Capital letter
+    this.radius = radius;
+    // this.getArea = function (){
+    //     return Math.PI * Math.pow(this.radius,2);
+    // };
+    console.log(this);
+}
+//Prototype - avoids instances/ objects having repeated methods
+Circle.prototype.getArea =function (){
+    return Math.PI * Math.pow(this.radius,2);
+};
+var myCircle = new Circle(10);
+console.log(myCircle);
+console.log(myCircle.getArea());
+
+var myOtherCircle = new Circle(30);
+console.log(myOtherCircle);
+console.log(myOtherCircle.getArea());
+
+
+
