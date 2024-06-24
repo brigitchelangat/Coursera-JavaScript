4230f38a636f1de6eb04f0956ceba7a4c585314e
chelangatbrigit <brigitchelangat@gmail.com>
2024-06-24 15:30:01 +0300

    Passing variables by value vs. by reference

--- Functions/functions.js
+++ Functions/functions.js
@@ -8,3 +8,21 @@ console.log(multiply(6,7));
 multiply.version = "v.1.0.0"; //Functions are objects
 console.log(multiply.toString());
 console.log(multiply.version);
+
+//Passing variables by value vs. by reference
+// 1. Pass by value
+var a = 7;
+var b = a;
+b = 5;
+console.log(a);
+console.log(b); //Result is 5 i.e changing b does not change a
+
+// 2. Pass by reference - Objects
+var a ={x:7};
+var b = a;
+console.log(a);
+console.log(b);
+//Changing b 
+b.x = 5;
+console.log(a);
+console.log(b);//Change in b affects a
