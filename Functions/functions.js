f1b1da4352ff339947311562d6e77f5149e977e3
chelangatbrigit <brigitchelangat@gmail.com>
2024-06-24 16:19:33 +0300

    Object Literals and 'this'

--- Functions/functions.js
+++ Functions/functions.js
@@ -58,4 +58,14 @@ console.log(myOtherCircle);
 console.log(myOtherCircle.getArea());
 
 
+//Object Literals and 'this'
+var literalCircle = {
+    radius: 20,
+    getArea: function (){
+        console.log(this);
+        return Math.PI * Math.pow(this.radius,2);
+    }
+};
+console.log(literalCircle.getArea());
+
 
