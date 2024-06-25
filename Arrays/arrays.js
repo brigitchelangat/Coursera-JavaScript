94021326a85738e25ab9e8562495da48cbbf0f5a
engenovic <engenovic@gmail.com>
2024-06-25 11:23:56 +0300

    Arrays

--- Arrays/arrays.js
+++ Arrays/arrays.js
@@ -11,4 +11,21 @@ console.log(array);
 array[2](array[0]); //calling the function at position 3
 console.log(array[3].course)
 
-//Shorthand notation for creating arrays
+//Shorthand notation for creating arrays - []
+var names = ["Brigit", "Brian", "Evans"];
+console.log(names);
+
+//looping through the array
+for(var i = 0; i < names.length; i++){
+    console.log("Hello " + names[i]);
+}
+names[100] = "Victor";
+for(var i = 0; i < names.length; i++){
+    console.log("Hello " + names[i]);
+}
+
+var names2 = ["Brigit", "Brian", "Evans"];
+
+for(var Nam in names2){
+    console.log("Hello " + names2[Nam])
+}
