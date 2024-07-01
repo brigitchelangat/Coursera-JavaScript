2ffc4b5efdae43b17812e2a769285e05f0c32549
chelangatbrigit <brigitchelangat@gmail.com>
2024-07-01 12:19:07 +0300

    DOMContentLoaded

--- DOM  Manipulation/dom.js
+++ DOM  Manipulation/dom.js
@@ -1,23 +1,31 @@
 //DOM Manipulation & Event Handling
+
+//DOMContentLoaded
+document.addEventListener("DOMContentLoaded",
+    function(event){
+        function sayHello(event) {
+            //console.log(this); - this points to the button
+            this.textContent = "Said it!";
+            var name = document.getElementById("name").value;
+            var message = "<h2>Hello " + name + "!</h2>";
+            console.log(message);
+        //     // Putting the message into the div whose id is content
+        //     document.getElementById("content").textContent = message;
+            document.getElementById("content").innerHTML = message;
+             if(name === "student"){
+                var title = document.querySelector("#title")
+                .textContent;
+                title += " & Lovin' it";
+                document.querySelector("#title").textContent = title
+             }
+        }
+        
+        // //Unobstrusive event handling - HTML doesn't need to know anything about your JS
+        // document.querySelector("button").addEventListener("click",sayHello);
+        document.querySelector("button").onclick =sayHello;
+        
+    }
+)
 console.log(document.getElementById("title"));
 console.log(document instanceof HTMLDocument);
 
-function sayHello(event) {
-    console.log(this);
-    var name = document.getElementById("name").value;
-    var message = "<h2>Hello " + name + "!</h2>";
-    console.log(message);
-//     // Putting the message into the div whose id is content
-//     document.getElementById("content").textContent = message;
-    document.getElementById("content").innerHTML = message;
-     if(name === "student"){
-        var title = document.querySelector("#title")
-        .textContent;
-        title += " & Lovin' it";
-        document.querySelector("#title").textContent = title
-     }
-}
-
-// //Unobstrusive event handling - HTML doesn't need to know anything about your JS
-// document.querySelector("button").addEventListener("click",sayHello);
-document.querySelector("button").onclick =sayHello;
