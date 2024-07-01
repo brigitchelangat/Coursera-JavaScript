7419ec071a907b12521eee823622e71256430aee
chelangatbrigit <brigitchelangat@gmail.com>
2024-07-01 11:37:59 +0300

    End of DOM manipulation

--- DOM  Manipulation/dom.js
+++ DOM  Manipulation/dom.js
@@ -1,3 +1,18 @@
 //DOM Manipulation
 console.log(document.getElementById("title"));
 console.log(document instanceof HTMLDocument);
+
+function sayHello() {
+    var name = document.getElementById("name").value;
+    var message = "<h2>Hello " + name + "!</h2>";
+    console.log(message);
+//     // Putting the message into the div whose id is content
+//     document.getElementById("content").textContent = message;
+    document.getElementById("content").innerHTML = message;
+     if(name === "student"){
+        var title = document.querySelector("#title")
+        .textContent;
+        title += " & Lovin' it";
+        document.querySelector("#title").textContent = title
+     }
+}
