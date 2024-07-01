49454706602e1f73a47d7673ac29afc2bea0bf1a
chelangatbrigit <brigitchelangat@gmail.com>
2024-07-01 12:32:15 +0300

    event argument

--- DOM  Manipulation/dom.js
+++ DOM  Manipulation/dom.js
@@ -4,6 +4,7 @@
 document.addEventListener("DOMContentLoaded",
     function(event){
         function sayHello(event) {
+            console.log(event);
             //console.log(this); - this points to the button
             this.textContent = "Said it!";
             var name = document.getElementById("name").value;
@@ -24,6 +25,15 @@ document.addEventListener("DOMContentLoaded",
         // document.querySelector("button").addEventListener("click",sayHello);
         document.querySelector("button").onclick =sayHello;
         
+        document.querySelector("body").addEventListener("mousemove",
+            function (event) {
+                if(event.shiftKey === true){
+                    console.log("x: " + event.clientX);
+                    console.log("y: " + event.clientY);
+                }
+               
+            }
+        )
     }
 )
 console.log(document.getElementById("title"));
